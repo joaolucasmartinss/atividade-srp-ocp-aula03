3fa1e084af165fe62daad6f02662141ccfcddae3
joaolucasmartinss <joaolucasmartins010@gmail.com>
2026-03-17 10:17:11 -0400

    Delete PagamentoBoleto.ts

--- PagamentoBoleto.ts
+++
@@ -1,8 +0,0 @@
-import { CalculoImposto } from "./CalculoImposto";
-
-export class ImpostoVestuario implements CalculoImposto {
-  calcular(preco: number, quantidade: number): number {
-    const total = preco * quantidade;
-    return total + total * 0.10;
-  }
-}
