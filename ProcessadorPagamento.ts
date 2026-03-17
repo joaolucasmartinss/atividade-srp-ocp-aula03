732eeff358637ce93f562f4c85c549ac5b6ddbee
joaolucasmartinss <joaolucasmartins010@gmail.com>
2026-03-17 10:16:38 -0400

    Delete ProcessadorPagamento.ts

--- ProcessadorPagamento.ts
+++
@@ -1,7 +0,0 @@
-import { ProcessadorPagamento } from "./ProcessadorPagamento";
-
-export class PagamentoCartao implements ProcessadorPagamento {
-  processar(valor: number): void {
-    console.log("Pagamento com cartão:", valor);
-  }
-}
