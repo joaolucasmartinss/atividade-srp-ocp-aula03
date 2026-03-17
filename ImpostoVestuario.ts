7f12ab5681542cede4e38a8964dd6c5efa3ef748
joaolucasmartinss <joaolucasmartins010@gmail.com>
2026-03-16 23:25:27 -0400

    Update ImpostoVestuario.ts

--- ImpostoVestuario.ts
+++ ImpostoVestuario.ts
@@ -1,1 +1,8 @@
-oi
+import { CalculoImposto } from "./CalculoImposto";
+
+export class ImpostoEletronico implements CalculoImposto {
+  calcular(preco: number, quantidade: number): number {
+    const total = preco * quantidade;
+    return total + total * 0.15;
+  }
+}
