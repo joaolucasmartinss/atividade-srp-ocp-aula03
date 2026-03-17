81bb5247463cbd51808a8463d26e85f17596f113
joaolucasmartinss <joaolucasmartins010@gmail.com>
2026-03-17 10:17:26 -0400

    Delete PagamentoPix.ts

--- PagamentoPix.ts
+++
@@ -1,7 +0,0 @@
-import { ProcessadorPagamento } from "./ProcessadorPagamento";
-
-export class PagamentoBoleto implements ProcessadorPagamento {
-  processar(valor: number): void {
-    console.log("Gerando boleto:", valor);
-  }
-}
