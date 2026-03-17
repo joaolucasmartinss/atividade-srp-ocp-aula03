ea84f6beaacfb9a87e70e63b25f2b44719d80aea
joaolucasmartinss <joaolucasmartins010@gmail.com>
2026-03-17 10:17:33 -0400

    Delete PedidoService.ts

--- PedidoService.ts
+++
@@ -1,7 +0,0 @@
-import { ProcessadorPagamento } from "./ProcessadorPagamento";
-
-export class PagamentoPix implements ProcessadorPagamento {
-  processar(valor: number): void {
-    console.log("Pagamento via PIX:", valor);
-  }
-}
