d54c997de2b7a459e88bff95ed23f0e902fb624a
joaolucasmartinss <joaolucasmartins010@gmail.com>
2026-03-17 10:16:03 -0400

    Delete ConfirmacaoEmail.ts

--- ConfirmacaoEmail.ts
+++
@@ -1,7 +0,0 @@
-import { ProcessadorPagamento } from "./ProcessadorPagamento";
-
-export class PagamentoCriptomoeda implements ProcessadorPagamento {
-  processar(valor: number): void {
-    console.log("Pagamento em criptomoeda:", valor);
-  }
-}
